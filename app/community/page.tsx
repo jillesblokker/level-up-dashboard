01768bb854b35b658a53c8a13f573dc90b3a442b
jillesblokker <jillesblokker@gmail.com>
2025-06-11 21:54:33 +0200

    community fix removed not used anymore

--- app/community/page.tsx
+++ app/community/page.tsx
@@ -1,11 +1,9 @@
 "use client"
 
-import { CommunityComponent } from "@/components/community-fix"
-
 export default function CommunityPage() {
   return (
     <div className="flex min-h-screen flex-col bg-black text-white">
-      <CommunityComponent />
+      {/* CommunityComponent removed: file missing. Add content or restore component if needed. */}
     </div>
   )
 }
