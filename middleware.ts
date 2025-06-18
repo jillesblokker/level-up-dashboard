8f6861e801e93bece5075889001d9ddc7b490774
jillesblokker <jillesblokker@gmail.com>
2025-06-19 00:11:21 +0200

    Temporarily disable Clerk middleware to test if it's causing the 500 error

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,21 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
 
-export default clerkMiddleware();
+export function middleware(request: NextRequest) {
+  // Temporarily disable Clerk middleware to test if it's causing the 500 error
+  return NextResponse.next()
+}
 
 export const config = {
   matcher: [
     /*
      * Match all request paths except for the ones starting with:
+     * - api (API routes)
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      * - public folder
      */
-    '/((?!_next/static|_next/image|favicon.ico|public/).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico|public/).*)',
   ],
 } 
