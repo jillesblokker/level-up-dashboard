7ba4cc05775238c432563b275a474bac639bc432
jillesblokker <jillesblokker@gmail.com>
2025-06-18 02:37:56 +0200

    fix: remove unused envSchema variable to fix build error

--- app/lib/env.ts
+++ app/lib/env.ts
@@ -1,14 +1,5 @@
 import { z } from "zod"
 
-const envSchema = z.object({
-  GITHUB_CLIENT_ID: z.string(),
-  GITHUB_CLIENT_SECRET: z.string(),
-  GITHUB_CALLBACK_URL: z.string().url(),
-  NEXTAUTH_URL: z.string().url(),
-  NEXTAUTH_SECRET: z.string(),
-  DATABASE_URL: z.string(),
-})
-
 export const env = {
   GITHUB_CLIENT_ID: process.env['GITHUB_ID'],
   GITHUB_CLIENT_SECRET: process.env['GITHUB_SECRET'],
