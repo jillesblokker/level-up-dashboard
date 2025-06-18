425fc5e83841107291393e716fe5226862e4d977
jillesblokker <jillesblokker@gmail.com>
2025-06-18 02:28:47 +0200

    remove unused useEffect and unused imports

--- app/friends/page.tsx
+++ app/friends/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState, useEffect } from "react"
+import { useState } from "react"
 import { Activity, Award, Bell, MessageCircleIcon as Message, Search, UserPlus } from "lucide-react"
 
 import { Button } from "@/components/ui/button"
