7fff647215a834531ac07db5855ec41fc8975832
jillesblokker <jillesblokker@gmail.com>
2025-07-06 21:11:36 +0200

    Add debug log to minimal Clerk SignIn page for production render check

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Test() {
+  if (typeof window !== 'undefined') {
+    console.log("Rendering Clerk SignIn page");
+  }
   return <SignIn />;
 }
 
