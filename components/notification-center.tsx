865b3c72bced8dbd05686c698ea69751a13c8ca1
jillesblokker <jillesblokker@gmail.com>
2025-07-11 00:26:02 +0200

    fix(notification-center): ensure correct image path and add width/height for no_mail.png

--- components/notification-center.tsx
+++ components/notification-center.tsx
@@ -103,7 +103,7 @@ export function NotificationCenter() {
           {notifications.length === 0 ? (
             <div className="flex justify-center items-center h-full p-8">
               <div className="flex flex-col items-center bg-gray-900/50 rounded-lg border border-gray-800 p-6 w-full max-w-xs mx-auto">
-                <img src="/images/Notifications/no_mail.png" alt="No mail" className="mx-auto h-32 w-32 my-4" />
+                <img src="/images/Notifications/no_mail.png" alt="No mail" className="mx-auto h-32 w-32 my-4" width={128} height={128} />
               </div>
             </div>
           ) : (
