25766286c6c043771a2192ab32a0052e7724a230
jillesblokker <jillesblokker@gmail.com>
2025-07-11 09:20:21 +0200

    fix(notification-center): use dash in image filename for no-mail.png and update src

--- components/notification-center.tsx
+++ components/notification-center.tsx
@@ -104,7 +104,7 @@ export function NotificationCenter() {
             <div className="flex justify-center items-center h-full p-8">
               <div className="flex flex-col items-center bg-gray-900/50 rounded-lg border border-gray-800 p-6 w-full max-w-xs mx-auto">
                 <img
-                  src="/images/Notifications/no_mail.png"
+                  src="/images/Notifications/no-mail.png"
                   alt="No mail"
                   className="mx-auto h-32 w-32 my-4"
                   width={128}
