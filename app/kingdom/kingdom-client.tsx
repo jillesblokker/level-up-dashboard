f3812f47aefdc7cf8bfb7d70924112a5037d9401
jillesblokker <jillesblokker@gmail.com>
2025-07-15 23:18:58 +0200

    Kingdom: Remove blue background from main content and fix wall image to always use /images/kingdom-tiles/Wall.png

--- app/kingdom/kingdom-client.tsx
+++ app/kingdom/kingdom-client.tsx
@@ -375,7 +375,7 @@ export function KingdomClient({ userId }: { userId: string | null }) {
   }
 
   return (
-    <div className="min-h-screen bg-gray-900 text-white">
+    <div className="min-h-screen">
       <HeaderSection
         title="KINGDOM"
         imageSrc={coverImage}
